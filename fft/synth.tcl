22e0f1ca02c6cded887eb572aad863531a819a68
Karthik B K <karthik.bk@incoresemi.com>
2024-05-01 14:18:21 +0530

    cleanup for git

--- fft/synth.tcl
+++
@@ -1,7 +0,0 @@
-create_project fft_bsv 
-set_top mkFFT
-add_files bscdir/mkFFT.v
-open_solution "solution1"
-set_part {xc7a35tcpg236-1} -tool vivado
-create_clock -period 10 -name default
-synth_design
